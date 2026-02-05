960d80755434a115e6ee983c86798e964fb33d87
Lucas Freitas <lucfr1991@gmail.com>
2026-02-05 20:06:56 -0300

    feat: add logical filter to send users

---
+++ src/mocks/users.js
@@ -0,0 +1,10 @@
+module.exports = [{
+    id: 1,
+    nome: 'Alex Bessa'
+}, {
+    id: 2,
+    nome: 'Valdiana Bessa'
+}, {
+    id: 3,
+    nome: 'Timóteo Bessa'
+}];
--- src/server.js
+++ src/server.js
@@ -1,7 +1,16 @@
 const http = require('node:http');
+const users = require('./mocks/users');
+
+
 
 http.createServer(function (request, response) {
     // console.log(request.url);
+    if (request.url === '/users') {
+        response.writeHead(200, {'content-type': 'application/json'});
+        response.end(JSON.stringify(users));
+        return;
+    }
+
     response.writeHead(200, {'content-type': 'text/html; charset=utf-8'});
     response.end('<h1>Hello, Client!</h1>');
 }).listen(3000);
